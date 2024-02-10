4a812a52f7fa1424ca4d1229c00b688578b12d98
Giovanni Borrelli <159608056+STUDIOBorrelli@users.noreply.github.com>
2024-02-10 08:27:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,28 +184,7 @@ with tab4:
     st.dataframe(fun_df()[fun_df()['ID.IMP']==selected_section+nomeric][options])   
     
 with tab5:
-    from fpdf import FPDF
-    import base64
     st.title(selected_section+nomeric)
-    st.subheader(fun_nome_incrocio(fun_filtro_risul(fun_df(),selected_section,nomeric)))
-    my_expander = st.expander(label='Espandimi')
-    with my_expander:
-        'Seleziona Opzioni da Stampare',options
 
-    report_text = st.text_input("Report Text")
-    export_as_pdf = st.button("Export Report")
-
-    def create_download_link(val, filename):
-        b64 = base64.b64encode(val)  # val looks like b'...'
-        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
-
-    if export_as_pdf:
-        
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font('Arial', 'B', 16)
-        pdf.cell(40, 10, report_text)
-        html = create_download_link(pdf.output(dest="S").encode(), "test")
-        st.markdown(html, unsafe_allow_html=True)
 
 # streamlit run strim.py
